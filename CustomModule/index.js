fa212fcea98daad3e5743946abca5d5c2cddb7f8
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-04-08 11:02:10 +0530

    custom module

---
+++ CustomModule/index.js
@@ -0,0 +1,8 @@
+const {add,sub, multi,division}= require("./myModule");
+ 
+console.log(add);
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(multi(5,5));
+console.log(division(5,5));
+
---
+++ CustomModule/myModule.js
@@ -0,0 +1,17 @@
+const add = (a,b)=>{
+    return a+b;
+}
+const sub = (a,b)=>{
+    return a-b;
+}
+const multi = (a,b)=>{
+    return a*b;
+}
+const division = (a,b)=>{
+    return a/b;
+}
+// module.exports.add=add
+// module.exports.sub=sub
+// module.exports.multi=multi
+
+module.exports={add,sub,multi,division}
