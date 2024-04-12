6fe08a3a525e484d8f716e51d713298fc1306ee6
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-04-12 16:59:15 +0530

    Module Wrapper in Node JS

---
+++ ModuleWrapper/wrap.js
@@ -0,0 +1,9 @@
+ 
+ 
+// each file in nodejs called module
+// all module have private
+// Behinf the scene node js wrap all code inside function
+(function(module, require,exports, __filename, __dirname){
+    var a = 10;
+    console.log(a);  
+})
