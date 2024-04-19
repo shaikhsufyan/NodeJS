7c594a4e2335e64be791e0c3e50fd5dbd049ac27
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-04-19 10:54:13 +0530

    main file

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// create file
+// fs.writeFileSync("readme.text", "Created By Sufyan Shaikh")
+// update file
+// fs.writeFileSync("readme.text", "Created By Sufyan Shaikh MY channel CODE WITH SUFYAN SHAIKH")
+
+// add data to file
+//  fs.appendFileSync("readme.text"," : SUBSCRIBE CHANNEL PLEASE")
+
+// Rename File Name
+// fs.renameSync('readme.text','readmes.txt');
+
+// Read Data
+// const buf_data = fs.readFileSync('readmes.txt');
+// org_data = buf_data.toString();
+// console.log(org_data);
+ 
+
+// add data to bio.txt
+fs.writeFileSync("bio.txt","Sufyan Bio Data");
